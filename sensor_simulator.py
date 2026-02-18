7250c6798b0e3e74cfb158cfa7d01f3baa2ea46e
nandakishoreg2006-ux <nandakishoreg2006@gmail.com>
2026-02-18 23:48:47 +0530

    Update sensor_simulator.py

--- sensor_simulator.py
+++ sensor_simulator.py
@@ -4,22 +4,15 @@ import requests
 
 FIREBASE_URL = "https://biochamber-52607-default-rtdb.firebaseio.com"
 
-def get_sensors():
-    response = requests.get(f"{FIREBASE_URL}/sensors.json")
-    return response.json()
-
-def update_prediction(data):
-    requests.put(f"{FIREBASE_URL}/prediction.json", json=data)
 while True:
     sensor_data = {
-        "pH": round(random.uniform(6.5,7.5),2),
-        "Temp": round(random.uniform(35,39),2),
-        "DO": round(random.uniform(75,90),2),
-        "OD": round(random.uniform(0.5,1.0),2)
+        "pH": round(random.uniform(6.5, 7.5), 2),
+        "Temp": round(random.uniform(35, 39), 2),
+        "DO": round(random.uniform(75, 90), 2),
+        "OD": round(random.uniform(0.5, 1.0), 2)
     }
 
-    db.reference("sensors").push(sensor_data)
+    requests.post(f"{FIREBASE_URL}/sensors.json", json=sensor_data)
     print("Sent:", sensor_data)
-
 
     time.sleep(5)
